942beb26879be6001136b87a2793363a10dd19ad
Daniel S Filho <104388781+Daniel-Souzax@users.noreply.github.com>
2022-04-27 17:19:50 -0300

    .

---
+++ S01/A20/index.js
@@ -0,0 +1,10 @@
+/* Aritiméticos 
++ Adição / Concatenação  
+** Potenciação 
+% Resto da divisão 
+*/
+
+const num1 = 10;
+const num2 = Number("5.2");
+console.log(num1 + num2);
+console.log(typeof num2);
---
+++ S01/A21/js/script.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt("digite um numero"));
+let num2 = Number(prompt("digite um numero"));
+let resultado = num1 + num2;
+
+// alert("a soma de " + num1 + " + " + num2 + " é igual a: " + resultado);
+alert(`a soma de ${num1} + ${num2} é igual a: ${resultado}`);
