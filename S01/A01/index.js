f735087ab37c90e4c10f58d4d3a39fb06e090204
Daniel S Filho <104388781+Daniel-Souzax@users.noreply.github.com>
2022-04-27 16:00:06 -0300

    s01

---
+++ S01/A01/index.js
@@ -0,0 +1,5 @@
+console.log(
+  'Meu nome é "NOME". Estou aprendendo JavaScript as',
+  10,
+  "da manhã"
+);
---
+++ S01/A19/index.js
@@ -0,0 +1,15 @@
+// String, number, undefined, null, boolean
+const nome = "Daniel"; // string
+const nome1 = "Daniel"; // string
+const nome2 = `Daniel`; // string
+const num1 = 10; // number
+const num2 = 10.52; //number
+let nomeAluno; // undefined -> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhuma na memória
+const aprovado = false; // Boolean true, false (lógico)
+
+let a = 2;
+const b = a;
+console.log(a, b);
+a = 3;
+console.log(a);
