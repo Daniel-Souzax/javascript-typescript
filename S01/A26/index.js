bb59f9a11a1968aa418e44170d5605ffb45af4aa
Daniel S Filho <104388781+Daniel-Souzax@users.noreply.github.com>
2022-04-28 17:29:42 -0300

    28/04/2022

---
+++ S01/A26/index.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 2.5;
+
+// console.log(num1.toString() + num2); concatenar
+// console.log(num1.toString(2)); valor binario
+// console.log(num1.toFixed(2)); arredonda o valor float
+// console.log(Number.isInteger(num1)); retorna com true e false se é interger ou não
+// let temp = num1 * "Ola";
+// console.log(Number.isNaN(temp)); retorna com true e false se ele é um NaN
+
+console.log(Number.isInteger(num1));
---
+++ S01/A27/index.js
@@ -0,0 +1,8 @@
+num1 = 10;
+// console.log(100 / 0);
+
+// let num2 = Math.floor(num1); Arredonda pra cima
+// let num2 = Math.ceil(num1); Arredonda pra baixo
+// console.log(Math.max (1,2,3,4,5,-10,-50, 1500, 9, 8, 7, 6)); Retorna o maior valor
+// console.log(Math.min(1,2,3,4,5,-10, -50, 1500, 9, 8, 7, 6)); Retorna o menor valor
+// const aleatorio = Math.round (Math.random()*(10 - 5) + 5); pega um numero aleatorio
