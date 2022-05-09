9bf0572dfe0c7d597b5d31145cfd25191095d3b9
Daniel S Filho <104388781+Daniel-Souzax@users.noreply.github.com>
2022-05-09 17:56:08 -0300

    09/05/2022

---
+++ S03 - Logica de programação/A44/assets/js/main.js
@@ -0,0 +1,23 @@
+// let peso = document.querySelector("#peso").value;
+// let altura = document.querySelector("#altura").value;
+
+function calcularIMC() {
+  let peso = document.querySelector("#peso").value;
+  let altura = document.querySelector("#altura").value;
+  let imc = (peso / (altura * altura)).toFixed(2);
+  let divresutlado = document.querySelector(".resultado");
+
+  if (imc < 18.5) {
+    divresutlado.innerHTML = `<p class="presultado">Seu IMC foi de ${imc} =  Abaixo do peso</p>`;
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    divresutlado.innerHTML = `<p class="presultado">Seu IMC foi de ${imc} = Peso normal</p>`;
+  } else if (imc >= 25 && imc <= 29.9) {
+    divresutlado.innerHTML = `<p class="presultado">Seu IMC foi de ${imc} = Sobrepeso</p>`;
+  } else if (imc >= 30 && imc <= 34.9) {
+    divresutlado.innerHTML = `<p class="presultado">Seu IMC foi de ${imc} = Obesidade grau 1</p>`;
+  } else if (imc >= 35 && imc <= 39.9) {
+    divresutlado.innerHTML = `<p class="presultado">Seu IMC foi de ${imc} = Obesidade grau 2</p>`;
+  } else if (imc >= 40) {
+    divresutlado.innerHTML = `<p class="presultado">Seu IMC foi de ${imc} = Obesidade grau 3</p>`;
+  }
+}
