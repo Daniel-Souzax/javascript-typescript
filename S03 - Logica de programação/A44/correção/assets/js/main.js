c4cfb298deb17b3d5637dabf331c118656a769d2
Daniel S Filho <104388781+Daniel-Souzax@users.noreply.github.com>
2022-05-10 17:20:48 -0300

    17:20

---
+++ S03 - Logica de programação/A44/correção/assets/js/main.js
@@ -0,0 +1,17 @@
+// Capturar evento de submit do formulario
+const form = document.querySelector("#formulario");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  setResultado("olá mundo");
+});
+
+function setResultado(msg) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = ``;
+  const p = document.createElement("p");
+  p.classList.add("paragrafo-resultado");
+  p.innerHTML = "Qualquer coisa";
+  resultado.appendChild(p);
+}
